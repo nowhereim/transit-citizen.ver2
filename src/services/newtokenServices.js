8f640e460e800edf99e5694d6db5adfb4573316e
AnTaeHwan <zyle1104@gmail.com>
2023-04-06 22:48:09 +0900

    :recycle: Refactor: 토큰재발급 세분화, 프로필 이미지 수정, 삭제 , 등록 세분화 , 기타 코드 간소화
    전반적인 구조변경

---
+++ src/services/newtokenServices.js
@@ -0,0 +1,56 @@
+const jwt = require("jsonwebtoken");
+const redis = require("../../utils/redis");
+const verify = (token) => {
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+class NewtokenServices {
+  constructor() {}
+
+  getNewtoken = async (token) => {
+    try {
+      const decoded = jwt.decode(token);
+      const resultacc = await redis.get(`${decoded.account}acc`);
+      if (resultacc !== token)
+        return {
+          errorMessage: "The last issued token does not match.",
+        };
+      const result = await redis.get(`${decoded.account}ref`);
+      if (!result) {
+        return {
+          errorMessage: "The refresh token has expired. Please login again.",
+        };
+      } else {
+        const refreshVerify = verify(result);
+        if (refreshVerify === false) {
+          return {
+            errorMessage: "The refresh token has expired. Please login again.",
+          };
+        } else {
+          const decoded = jwt.decode(result);
+          const token = jwt.sign(
+            {
+              account: decoded.account,
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: "1m",
+            },
+          );
+          redis.set(`${decoded.account}acc`, token);
+          console.log(token);
+          return token;
+        }
+      }
+    } catch (error) {
+      console.log(error.name);
+      console.log(error.message);
+    }
+  };
+}
+
+module.exports = NewtokenServices;
